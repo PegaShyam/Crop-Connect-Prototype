999d5d124cf6ede8ba0bc7ab136551d5120040d3
PegaShyam <shyamprasadmurugan@gmail.com>
2023-06-16 02:02:13 +0530

    responsive app

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,17 +1,16 @@
-// Function used to shrink nav bar removing paddings and adding black background 
-$(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-        $('.nav').addClass('affix');
-        console.log("OK");
-    } else {
-        $('.nav').removeClass('affix');
-    }
-});
+// For toggling and finding number of children and other stuff is done here!
 
-$('.navTrigger').click(function () {
-    $(this).toggleClass('active');
-    console.log("Clicked menu");
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
+const navigation = document.getElementById("nav");
+const menu = document.getElementById("menu");
 
+menu.addEventListener("click", () => {
+  // The navigation.children.length means the following :-
+  // The children inside a parent are basically an array of elements; So, here I'm finding the length of the array aka how many children are inside the nav bar.
+  //   Yup That's all.
+  navigation.style.setProperty("--childenNumber", navigation.children.length);
+
+  //    Casually Toggling Classes to make them animate on click
+  //   Regular stuff ;)
+  navigation.classList.toggle("active");
+  menu.classList.toggle("active");
 });
